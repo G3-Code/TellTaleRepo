dbeb81730421f70d2e990a595d013c3d925ecf2a
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-15 22:06:08 -0500

    TellTaleRepo - OAuth integration - initial
    Added passport.js and strategy for google integration

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
-
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ message: `Hi there!!` });
-});
+// app.get("/", (req, res) => {
+//   res.send({ message: `Hi there!!` });
+// });
+//http://localhost:5000/auth/google/callback
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
