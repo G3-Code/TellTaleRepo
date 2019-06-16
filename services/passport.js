67fa568641675484b06b13f6f450dc1f60d968c9
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-16 18:48:17 -0500

    TellTaleApp - Completed logout
    Tested login, authentication, cookie creation and logout

--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,16 @@ const keys = require("../config/keys");
 //const User = require("../models/user");
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
