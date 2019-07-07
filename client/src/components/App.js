445b935dde6691a16c6e9d96e4460a59d3c44f89
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-07 16:33:02 -0500

    Creating the Client and setting up redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <div>Hi there!</div>
         <a href="/auth/google">Sign in with google!</a>
       </header>
