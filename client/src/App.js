c5b8db4e396fc0aa9d8afe9db35de7d8fcf59905
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-07 08:38:09 -0500

    Initial client set up

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Hi there!</div>
+        <a href="/auth/google">Sign in with google!</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
