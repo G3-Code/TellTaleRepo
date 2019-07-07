81208cd9b0a08b4022f648b60566bbbf91aa25ad
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-07 17:03:02 -0500

    Setting up the basic path routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 function App() {
   return (
     <div>
-      <header>
-        <div>Hi there!</div>
-        <a href="/auth/google">Sign in with google!</a>
-      </header>
+      <Router>
+        <div>
+          <Route exact={true} path="/" component={Landing} />
+          <Route path="/surveys" component={Dashboard} />
+        </div>
+      </Router>
     </div>
   );
 }
