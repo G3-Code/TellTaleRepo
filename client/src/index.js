6e22f9bb8d41524e365cc795bb44653f8ebf47ec
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-07 20:27:29 -0500

    Added logger and composeEnhancer for logging and adding middlewares

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,19 @@ import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import reduxThunk from "redux-thunk";
+import logger from "redux-logger";
 
 import App from "./components/App";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(applyMiddleware(reduxThunk, logger))
+);
 
 ReactDOM.render(
   <Provider store={store}>
